8c0728a7bf4ebd01e7ab98127e3212ced229cd87
caw190 <56939619+caw190@users.noreply.github.com>
2021-03-30 18:27:40 -0400

    Add files via upload

---
+++ Blink.ino
@@ -0,0 +1,22 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void){
+  DDRB = 0xFF; // Sets PB0 and PB1 to outputs
+  DDRD = 0x00;
+  while(1){
+    if(PIND & (1<<PD0)){
+      PORTB = PORTB | (1<<PB0); // Turns on only PB0
+      _delay_ms(100);
+      PORTB = PORTB & ~(1<<PB0);
+      _delay_ms(100); 
+    }
+    if(PIND & (1<<PD1)){
+      PORTB = PORTB | (1<<1); // Turns on only PB1
+      _delay_ms(100); 
+      PORTB = PORTB & ~(1<<1);
+      _delay_ms(100);
+    }
+  }
+  return(0);
+}
